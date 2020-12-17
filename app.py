82437c95af4f6ba3de611f8ab3f7e56edc46f834
一只追逐蜻蜓的小猫猫 <digdongaa@gmail.com>
2020-12-17 14:52:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+import subprocess
+import os
+subprocess.run(["df", "-h"])
+print(os.listdir('/mnt/workspace'))
+print(os.stat('/mnt/workspace'))
+
 @app.route('/', methods=['GET'])
 def hello():
     return 'Hello FunctionCompute, http function\n'
