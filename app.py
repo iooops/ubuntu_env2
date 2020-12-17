ea251ad21bdd98c8a9d0125307736c83aeae86a0
一只追逐蜻蜓的小猫猫 <digdongaa@gmail.com>
2020-12-17 14:56:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ subprocess.run(["df", "-h"])
 print(os.listdir('/mnt/workspace'))
 print(os.stat('/mnt/workspace'))
 
-@app.route('/', methods=['GET'])
+@app.route('/2016-08-15/proxy/sounbum/ubuntu_flask/', methods=['GET'])
 def hello():
     return 'Hello FunctionCompute, http function\n'
 
